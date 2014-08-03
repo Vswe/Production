7554bfc65fb0af3783eb5ef678c4aa6a1a5bca43
Vswe <vidsw926@student.liu.se>
2014-08-03 08:16:34 +0200

    Fixed shift clicking

--- src/main/java/vswe/production/gui/container/ContainerTable.java
+++ src/main/java/vswe/production/gui/container/ContainerTable.java
@@ -5,6 +5,7 @@ import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.InventoryPlayer;
 import net.minecraft.inventory.Container;
 import net.minecraft.inventory.Slot;
+import net.minecraft.item.ItemStack;
 import vswe.production.tileentity.TileEntityTable;
 
 public class ContainerTable extends Container {
@@ -35,5 +36,114 @@ public class ContainerTable extends Container {
     @Override
     public boolean canInteractWith(EntityPlayer player) {
         return table.isUseableByPlayer(player);
+    }
+
+
+
+
+    @Override
+    public ItemStack transferStackInSlot(EntityPlayer player, int i) {
+        ItemStack itemstack = null;
+        Slot slot = (Slot)inventorySlots.get(i);
+        if(slot != null && slot.getHasStack()) {
+            ItemStack slotItem = slot.getStack();
+            itemstack = slotItem.copy();
+            if(i < table.getSizeInventory()) {
+                if(!mergeItemStack(slotItem, table.getSizeInventory() + 28, table.getSizeInventory() + 36, false)) {
+                    if(!mergeItemStack(slotItem, table.getSizeInventory(), table.getSizeInventory() + 28, false)) {
+                        return null;
+                    }
+                }
+            }else if(!mergeItemStack(slotItem, 0, table.getSizeInventory(), false)){
+                return null;
+            }
+            if(slotItem.stackSize == 0){
+                slot.putStack(null);
+            }else{
+                slot.onSlotChanged();
+            }
+            if(slotItem.stackSize != itemstack.stackSize){
+                slot.onPickupFromSlot(player,slotItem);
+            }else{
+                return null;
+            }
+        }
+        return itemstack;
+    }
+
+    @Override
+    protected boolean mergeItemStack(ItemStack item, int start, int end, boolean invert){
+        boolean result = false;
+        int id = start;
+
+        if (invert) {
+            id = end - 1;
+        }
+
+        Slot slot;
+        ItemStack slotItem;
+
+        if (item.isStackable()) {
+            while (item.stackSize > 0 && (!invert && id < end || invert && id >= start)) {
+                slot = (Slot)this.inventorySlots.get(id);
+                slotItem = slot.getStack();
+
+                if (slotItem != null && slotItem.stackSize > 0 && slotItem.getItem() == item.getItem() && (!item.getHasSubtypes() || item.getItemDamage() == slotItem.getItemDamage()) && ItemStack.areItemStackTagsEqual(item, slotItem)) {
+                    int size = slotItem.stackSize + item.stackSize;
+
+                    int maxLimit = Math.min(item.getMaxStackSize(), slot.getSlotStackLimit());
+                    if (size <= maxLimit) {
+                        item.stackSize = 0;
+                        slotItem.stackSize = size;
+                        slot.onSlotChanged();
+                        result = true;
+                    }else if (slotItem.stackSize < maxLimit) {
+                        item.stackSize -= maxLimit - slotItem.stackSize;
+                        slotItem.stackSize = maxLimit;
+                        slot.onSlotChanged();
+                        result = true;
+                    }
+                }
+
+                if (invert) {
+                    --id;
+                }else{
+                    ++id;
+                }
+            }
+        }
+
+        if (item.stackSize > 0){
+            if (invert){
+                id = end - 1;
+            }else{
+                id = start;
+            }
+
+            while (!invert && id < end || invert && id >= start){
+                slot = (Slot)this.inventorySlots.get(id);
+                slotItem = slot.getStack();
+
+                if (slotItem == null && slot.isItemValid(item)) {
+                    int stackSize = Math.min(slot.getSlotStackLimit(), item.stackSize);
+                    ItemStack newItem = item.copy();
+                    newItem.stackSize = stackSize;
+                    item.stackSize -= stackSize;
+                    slot.putStack(newItem);
+                    slot.onSlotChanged();
+
+                    result = item.stackSize == 0;
+                    break;
+                }
+
+                if (invert){
+                    --id;
+                }else{
+                    ++id;
+                }
+            }
+        }
+
+        return result;
     }
 }
